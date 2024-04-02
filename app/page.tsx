5eb1fa6dec5271b6ae4b78c3c8cf40c46b0fdda2
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-02 13:43:41 -0300

    sign-in/up clerk pages and shadcn components installation

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex bg-white text-black min-h-screen flex-col items-center justify-between p-24">
+      <h1>HTTPie</h1>
     </main>
   );
 }
